dbc0b7d24a398b9dac2fbac8a2a184f0b6e00e28
Martin Simpson <martin.simpson@ubiquitypress.com>
2024-02-04 14:20:44 +0700

    Added missing resources for UNA

--- app/controllers/hyrax/una_open_educational_resources_controller.rb
+++ app/controllers/hyrax/una_open_educational_resources_controller.rb
@@ -3,7 +3,7 @@ module Hyrax
   class UnaOpenEducationalResourcesController < ApplicationController
     include Hyrax::WorksControllerBehavior
     include Hyrax::BreadcrumbsForWorks
-    include Hyku::Schema::WorksControllerBehavior
+    include Hyku::Plugin::Schema::Yaml::WorksControllerBehavior
 
     self.curation_concern_type = ::UnaOpenEducationalResource
     self.show_presenter = Hyrax::UnaOpenEducationalResourcePresenter
