09c1fb6eb08018b47a9e4910825ff137d8bf2785
Martin Simpson <martin.simpson@ubiquitypress.com>
2024-02-07 22:01:13 +0700

    Fixed UNA education resource

--- app/presenters/hyrax/una_open_educational_resource_presenter.rb
+++ app/presenters/hyrax/una_open_educational_resource_presenter.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Hyrax
   class UnaOpenEducationalResourcePresenter < Hyrax::WorkShowPresenter
-    include ::Hyku::Pluin::Schema::Yaml::Presenter(:una_open_educational_resource)
+    include ::Hyku::Plugin::Schema::Yaml::Presenter(:una_open_educational_resource)
     include ::HykuKnapsack::WorkPresenterBehavior
     include ::HykuKnapsack::PresenterDelegatable
   end
